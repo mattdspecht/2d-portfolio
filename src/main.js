4c43ae2b1a4fd9998e9e743c8c1c1e02e37b2fb3
matspe24 <91612588+matspe24@users.noreply.github.com>
2025-01-04 20:53:08 -0500

    map added, obselete classes removed, setup complete-ish
    used Tiled

--- src/main.js
+++ src/main.js
@@ -1,24 +1,14 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import { k } from "./kaboomCtx";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+k.loadSprite("spritesheet", "spritesheet.png", {
+    sliceX: 39,
+    sliceY: 31,
+    anims: {
+        "idle-down": 936,
+        "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+        "idle-side": 975,
+        "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+        "idle-up": 1014,
+        "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+    },
+});
