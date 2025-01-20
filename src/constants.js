256cfb29cd489508e8998771f2d2c1f6d80405d5
matspe24 <matthewdspecht@gmail.com>
2025-01-20 17:33:04 -0500

    dialogue finished
    project done! (for now?)

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,17 @@
 export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC, my friend helped me build it. I use it for coding, gaming, and watching movies.
+    Here is my <a href="https://www.linkedin.com/in/matthew-specht/" target="_blank">LinkedIn</a>!`,
+  "cs-degree": `I haven't earned my degree yet, here's where I'll hang it up when I'm done. I'm currently studying Computer Science and Mathematics at Rutgers University.`,
+  "sofa-table": `That's my sofa. I like to relax here after my classes.`,
+  tv: `That's my TV. I've been watching tech YouTubers a lot recently like Neetcode, Mental Outlaw, and Sajjaad Khader.`,
+  bed: `This where I scroll on my phone for an hour every night before going to sleep.`,
+  resume: `This is my desk and on it is my resume. <a href="https://docs.google.com/document/d/12CiGjaMX3WwksnLfJoSFcwkPSULfu8nr/edit?usp=sharing&ouid=110046454459779822514&rtpof=true&sd=true" target="_blank">Check it out!</a>
+  Contact me at matthewdspecht@gmail.com for any job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS.`,
+  library: `There are a lot of books on my shelves, most of which I will probably never finish.
+  I'm currently reading Thinking, Fast and Slow by Daniel Kahneman, and it is very interesting.`,
+  exit: `The door is locked, you're stuck here forever. (Just close the tab when you want to leave :D)`,
+};
--- src/utils.js
+++ src/utils.js
@@ -35,3 +35,13 @@ export function displayDialogue(text, onDisplayEnd) {
         }
     });
 }
+
+export function setCamScale(k) { 
+    const resizeFactor = k.width() / k.height();
+    if (resizeFactor < 1) {
+        k.camScale(k.vec2(1));
+        return
+    };
+
+    k.camScale(k.vec2(1.5));
+}
